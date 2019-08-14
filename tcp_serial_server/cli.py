064aca6d4df1d407fd967fadf63e93de7a6bda18
azcoigreach <azcoigreach@gmail.com>
2019-08-14 14:12:21 -0700

    commit 1412

--- tcp_serial_server/cli.py
+++ tcp_serial_server/cli.py
@@ -26,29 +26,37 @@ pass_context = click.make_pass_decorator(Context, ensure=True)
 @click.command()
 def cli():
     '''TCP Server Socket'''
-
-    TCP_IP = '0.0.0.0'
-    TCP_PORT = 42000
-    BUFFER_SIZE = 1024
+    
     FILENAME = 'server.pickle'
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((TCP_IP, TCP_PORT))
-    s.listen(1)
-    logger.info('Socket %s:%s open', TCP_IP, TCP_PORT)
-    conn, addr = s.accept()
-    logger.info('Connection from: %s', addr)
-    while 1:
-        data = conn.recv(BUFFER_SIZE)
-        if not data:
-            break
-        logger.info('received data: %s', data)
-        if data:
-            with open(FILENAME, 'wb') as f:
-                pickle.dump(data, f)
+    def socket_server():
+        TCP_IP = '0.0.0.0'
+        TCP_PORT = 42000
+        BUFFER_SIZE = 1024
 
-        # else:
-        #     pass
-            
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((TCP_IP, TCP_PORT))
+        logger.info('Socket %s:%s open', TCP_IP, TCP_PORT)
+        
+        s.listen(1)
+        conn, addr = s.accept()
+        logger.info('Connection from: %s', addr)
+        
+        while 1:
+            data = conn.recv(BUFFER_SIZE)
+            if not data:
+                break
+            logger.info('received data: %s', data)
+            if data:
+                return data
+            else:
+                pass
+
+    while True:
+        with open(FILENAME, 'wb') as f:
+            data = socket_server()
+            pickle.dump(data, f)
+        
+
     logger.debug('Socket closed.')
     conn.close()
