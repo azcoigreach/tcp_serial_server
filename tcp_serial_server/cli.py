3515ac7782a02055e7e68fdbcdac63c5aa03ce3c
azcoigreach <azcoigreach@gmail.com>
2019-08-14 13:38:40 -0700

    Pickle test

--- tcp_serial_server/cli.py
+++ tcp_serial_server/cli.py
@@ -6,6 +6,7 @@ import logging
 import coloredlogs
 import socket
 import time
+import pickle
 
 '''
 Project: TCP Socket Serial Server
@@ -24,5 +25,30 @@ pass_context = click.make_pass_decorator(Context, ensure=True)
 
 @click.command()
 def cli():
-    pass
+    '''TCP Server Socket'''
 
+    TCP_IP = '0.0.0.0'
+    TCP_PORT = 42000
+    BUFFER_SIZE = 1024
+    FILENAME = 'server.pickle'
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((TCP_IP, TCP_PORT))
+    s.listen(1)
+    logger.info('Socket %s:%s open', TCP_IP, TCP_PORT)
+    conn, addr = s.accept()
+    logger.info('Connection from: %s', addr)
+    while 1:
+        data = conn.recv(BUFFER_SIZE)
+        if not data:
+            break
+        logger.info('received data: %s', data)
+        
+        with open(FILENAME, ‘wb’) as f:
+            pickle.dump(data, f)
+
+        else:
+            pass
+            
+    logger.debug('Socket closed.')
+    conn.close()
